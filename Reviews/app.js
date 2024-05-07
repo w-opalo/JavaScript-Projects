7ac9e0e4f88a5bfb1bf54b1b51ba1b978a1fdf99
w-opalo <131133776+w-opalo@users.noreply.github.com>
2024-05-07 11:51:34 +0300

    Update app.js
    reviews changes

--- Reviews/app.js
+++ Reviews/app.js
@@ -30,4 +30,18 @@ const reviews = [
   },
 ];
 
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//starting Item
+let currentItem = 0;
+
+
 
