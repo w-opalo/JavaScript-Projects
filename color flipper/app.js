cc4ece6183e6679094ba774f7187c6354eee34c7
w-opalo <131133776+w-opalo@users.noreply.github.com>
2024-06-02 16:26:25 +0300

    Update app.js

--- color flipper/app.js
+++ color flipper/app.js
@@ -1,14 +1,22 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const color = document.querySelector('.color'),
- btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', function(){
-    const randomNumber = getRandomNumber();
-    // console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-});
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+    // console.log(btn);
+    // const randomNumber = getRandomColor();
+
+    document.body.style.backgroundColor = colors[getRandomColor()];
+    color.textContent = colors[getRandomColor()];
+})
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
 }
+
+
+
+
+
 
